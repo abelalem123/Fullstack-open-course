0e4b5122f6e613921eae254f05bdde44128e436e
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-11-25 10:51:08 +0300

    part2 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,145 @@
+import { useState ,useEffect} from 'react'
+import axios from 'axios'
+import noteService from './services/persons'
+
+const Notification = ({ notf,error }) => {
+  if (notf||error === null) {
+    return null
+  }
+else if(notf!==null){
+
+  return (
+    <div className='added'>
+      {notf}
+    </div>
+  )
+}
+else if(error!==null){
+
+  return (
+    <div className='error'>
+      {error}
+    </div>
+  )
+}
+}
+const Filter=(props)=>{
+  return(
+    <form>
+    <div>
+      filter shown with <input value={props.searchItem} onChange={(event)=>props.setNewSearch(event.target.value)}/>
+    </div>
+    <button onClick={(e)=>{
+      e.preventDefault();
+    props.setSearchedArray( props.persons.filter((person)=>person.name.toUpperCase().includes(props.searchItem.toUpperCase())))
+    }} type='submit'>search</button>
+  </form>
+  )
+}
+const PersonForm=({newName,newNumber,persons,setNewNumber,setNewName,setPersons,setSearchedArray,setNotificationMessage,setError})=>{
+return(
+   <form>
+        <div>
+          name: <input value={newName} onChange={(event)=>{
+            console.log(event.target.value);
+            setNewName(event.target.value)
+          }}/>
+        </div>
+        <div>number: <input value={newNumber} onChange={(event)=>setNewNumber(event.target.value)} /></div>
+        <div>
+          <button onClick={(e)=>{
+            e.preventDefault()
+            const duplicates=persons.filter((person)=>person.name.toUpperCase()===newName.toUpperCase())
+            if( duplicates.length>0){
+             
+              if(window.confirm(`${newName} is already added to phonebook, replace the old number with new one?`)){
+                const firstperson=duplicates[0];
+                const person={...firstperson,number:newNumber}
+           noteService.update(firstperson.id, person).then((response)=>{
+            console.log(response);
+              setPersons(persons.map((n)=>n.id!==firstperson.id?n:response));
+              setSearchedArray(persons.map((n)=>n.id!==firstperson.id?n:response))
+            }).catch((error)=>{setError(`Information of ${person.name} is removed from server`)
+            setTimeout(() => {
+              setError(null)
+            }, 5000)
+          })
+              }
+            }
+           else{
+            const person={name:newName,number:newNumber}
+           noteService.create(person).then((response)=>{
+            setNotificationMessage(
+              `Added ${response.name}`
+            )
+            setTimeout(() => {
+              setNotificationMessage(null)
+            }, 5000)
+              setPersons(persons.concat(response));
+              setSearchedArray(persons.concat(response))
+            })
+          
+           }
+            
+            setNewName('')
+            setNewNumber('')
+          }} type="submit">add</button>
+        </div>
+      </form>
+)
+}
+const Persons=({searchePersons,set})=>{
+  const general=(id)=>{
+    const validate=()=>{
+      if(window.confirm('are you sure?')){
+       return noteService.remove(id).then(()=>{
+     set(searchePersons.filter((p)=>p.id!==id))
+       })
+      }
+      else{
+        return console.log('hey');
+      }
+    }
+    return validate
+  }
+  return(
+    <div>{searchePersons.map((person)=><div key={person.id}>{person.name} {person.number} <button onClick={general(person.id)}>delete</button></div>)} </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const[newNumber,setNewNumber]=useState('')
+  const[searchItem,setNewSearch]=useState('')
+  const[searchePersons,setSearchedArray]=useState([]);
+  const [notfMessage, setNotificationMessage] = useState(null)
+  const [errorm, setError] = useState(null)
+
+
+  
+useEffect(()=>{
+  console.log('effects');
+  noteService.getAll().then((response)=>{
+    console.log('promise fulfilled');
+    setPersons(response)
+    setSearchedArray(response)
+  })
+},[])
+console.log('renders',persons.length);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification notf={notfMessage} error={errorm}/>
+      <Filter persons={persons} searchItem={searchItem} setSearchedArray={setSearchedArray} setNewSearch={setNewSearch}/>
+      <h2>add new</h2>
+     <PersonForm newName={newName} newNumber={newNumber} persons={persons} setError={setError}  setNotificationMessage={setNotificationMessage} setNewNumber={setNewNumber} setNewName={setNewName} setPersons={setPersons} setSearchedArray={setSearchedArray}/>
+      <h2>Numbers</h2>
+     <Persons searchePersons={searchePersons} set={setSearchedArray}/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll=()=>{
+   return axios.get(baseUrl).then((response)=>response.data)
+}
+const create=(newObject)=>{
+    return axios.post(baseUrl,newObject).then((response)=>response.data)
+}
+const remove=(id)=>{
+    return axios.delete(`${baseUrl}/${id}`).then(()=>console.log('deleted'))
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
+  
+
+
+export default {getAll,create,remove,update}
